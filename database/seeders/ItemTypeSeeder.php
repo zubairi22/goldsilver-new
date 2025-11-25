432435b930125c849600f4197d0c2cf75d1e9374
Z <zubairiyhy22@gmail.com>
2025-11-25 12:30:22 +0800

    feat : revamp 1

---
+++ database/seeders/ItemTypeSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\ItemType;
+
+class ItemTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $types = [
+            'Anting',
+            'Bangle Anak',
+            'Cincin Anak',
+            'Cincin Dewasa',
+            'Gelang Anak',
+            'Gelang Bangle',
+            'Gelang Kaki',
+            'Gelang Keroncong',
+            'Gelang Plat',
+            'Gelang Rantai',
+            'Kalung Anak',
+            'Kalung Dewasa',
+            'Koyek',
+            'Liontin',
+        ];
+
+        foreach ($types as $type) {
+            ItemType::create([
+                'name' => $type,
+            ]);
+        }
+    }
+}
