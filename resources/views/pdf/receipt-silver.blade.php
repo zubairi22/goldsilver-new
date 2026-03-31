27547657a29198d9f134910d8c2d6df047d954ad
Z <zubairiyhy22@gmail.com>
2026-03-31 16:43:36 +0800

    feat : change print into a4

--- resources/views/pdf/receipt-silver.blade.php
+++ resources/views/pdf/receipt-silver.blade.php
@@ -121,7 +121,7 @@
                     <tbody>
                         @foreach ($sale->items as $item)
                             <tr>
-                                <td style="border:1px solid #ccc; text-align:center; padding:4px; height: 250px;">
+                                <td style="border:1px solid #ccc; text-align:center; padding:4px; height: 220px;">
                                     @if ($sale->sale_image_path)
                                         <img src="{{ $sale->sale_image_path }}" width="200">
                                     @else
